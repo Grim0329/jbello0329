9a55276e9ffdb537b5d8f3f6c5d05f0a88b31d24
Jonathan Bello <thanescobar29@gmail.com>
2026-02-28 09:34:51 +0800

    Create Footer.tsx

---
+++ Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='bg-gray-800 text-white p-4 nt-4'>
+       <p className= 'text-center'>&copy; 2024 Grabe ba App. All rights reserved.</p>
+         </div>
+  )
+}
