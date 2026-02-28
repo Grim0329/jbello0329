1593b8ac087c451d09c95ac6d65e9bb08008d1de
Jonathan Bello <thanescobar29@gmail.com>
2026-02-28 10:12:10 +0800

    Delete Footer.tsx

--- Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div className='bg-gray-800 text-white p-4 nt-4'>
-       <p className= 'text-center'>&copy; 2024 Grabe ba App. All rights reserved.</p>
-         </div>
-  )
-}
